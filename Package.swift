1370ac94a116695d427206ae2e9ffe74840205c3
Vidhi Gavadia <62442249+vigavadi@users.noreply.github.com>
2024-09-19 11:13:09 -0400

    test package

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "IntuneMAM",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "IntuneMAMSwift",
+                 targets: ["IntuneMAMSwift", "IntuneMAMSwiftStub"]),
+        .library(name: "IntuneMAMStatic",
+                 targets: ["IntuneMAMStatic", "IntuneMAMSwiftStub"]),
+        .library(name: "IntuneMAMTelemetry",
+                 targets: ["IntuneMAMTelemetry"]),
+        .library(name: "libIntuneMAMSwiftFileProvider",
+                 targets: ["libIntuneMAMSwiftFileProvider", "IntuneMAMSwiftStub"])
+    ],
+    targets: [
+
+        .target(
+            name: "IntuneMAMStatic",
+            dependencies: [
+                .target(name: "IntuneMAMStaticBinary")
+            ],
+            linkerSettings: [
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("CoreServices"),
+                .linkedFramework("ImageIO"),
+                .linkedFramework("LocalAuthentication"),
+                .linkedFramework("MessageUI"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("Security"),
+                .linkedFramework("MetricKit"),
+                .linkedFramework("MessageUI"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("WebKit"), 
+                .linkedLibrary("libsqlite3.tbd"),
+                .linkedLibrary("libc++.tbd")
+            ]
+        ),
+        // Local Binary Packages
+        .binaryTarget(name: "IntuneMAMSwiftStub",
+                      path: "./IntuneMAMSwiftStub.xcframework"
+                     ),
+        .binaryTarget(name: "IntuneMAMSwift",
+                      path: "./IntuneMAMSwift.xcframework"
+                     ),
+        .binaryTarget(name: "IntuneMAMStaticBinary",
+                      path: "./IntuneMAMStatic.xcframework"
+                     ),
+        .binaryTarget(name: "IntuneMAMTelemetry",
+                      path: "./IntuneMAMTelemetry.xcframework"
+                     ),
+        .binaryTarget(name: "libIntuneMAMSwiftFileProvider",
+                      path: "./libIntuneMAMSwiftFileProvider.xcframework"
+                     )
+    ]
+)
